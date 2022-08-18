c337a158bcfb34727597e269617ddb9c0df90841
kozhevnikov <kozhevnikov.alex-2000@mail.ru>
2022-08-18 22:44:10 +0300

    update version to 2.1

--- src/main/java/ru/kozhevnikov/course/controllers/PeopleController.java
+++ src/main/java/ru/kozhevnikov/course/controllers/PeopleController.java
@@ -1,11 +1,15 @@
 package ru.kozhevnikov.course.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.kozhevnikov.course.dao.PersonDAO;
 import ru.kozhevnikov.course.models.Person;
+
+import java.sql.SQLException;
 
 @Controller
 @RequestMapping("/people")
@@ -32,7 +36,12 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "people/new";
+        }
+
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -44,7 +53,12 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
--- src/main/java/ru/kozhevnikov/course/dao/PersonDAO.java
+++ src/main/java/ru/kozhevnikov/course/dao/PersonDAO.java
@@ -1,47 +1,44 @@
 package ru.kozhevnikov.course.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.kozhevnikov.course.models.Person;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
-    private List<Person> people;
 
-    {
-        people = new ArrayList<>();
+    private JdbcTemplate jdbcTemplate;
 
-        people.add(new Person(++PEOPLE_COUNT, "Alex"));
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Harry"));
-        people.add(new Person(++PEOPLE_COUNT, "Harry2"));
-        people.add(new Person(++PEOPLE_COUNT, "Harry3"));
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<Person> index() {
-        return people;
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null); // needs to change to orElseThrow 'UnitNotFoundException(description)
     }
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)", person.getName(), person.getAge(),
+                person.getEmail());
     }
 
     public void update(int id, Person person) {
-//        people.get(id).setName(person.getName());
-        Person personToBeUpdate = show(id);
-        personToBeUpdate.setName(person.getName());
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id = ?", person.getName(), person.getAge(),
+                person.getEmail(), id);
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
     }
 }
 
